c493512b2a000e9f6e32fea0cfd88d87d352475e
Gabriel Borges <gabriel.borges@sap.com>
2018-09-30 13:12:17 -0300

    create decompress

--- trabalho.py
+++ trabalho.py
@@ -35,32 +35,47 @@ def pack_smaller(first, huffman):
 
 CHUNK = 2 ** 10
 filename = sys.argv[1]
-if args.is_encode:
-    with open(args.input_filename, 'rb') as input_file:
-       pedaco = input_file.read(CHUNK)
-       output = []
-       while pedaco:
-          # pdb.set_trace()
-           ped_lz4 = {
-               'a': 4,
-               'd': lz4.frame.compress(pedaco)
-           }
-           codec_huff = HuffmanCodec.from_data(pedaco)
-           ped_huff = {
-               'a': 1,
-               'd': codec_huff.encode(pedaco),
-               't': codec_huff.get_code_table()
-           }
-           small_dict = pack_smaller(ped_lz4, ped_huff)
-           output.append(small_dict)
-           #do stuff
+with open(args.input_filename, 'rb') as input_file:
+    if args.is_encode:
            pedaco = input_file.read(CHUNK)
-       with open(args.output_filename, 'wb') as output_file:
-           umsgpack.pack(output, output_file, ext_handlers=handlers)
-else:
-    #apaga esse pass e faz ele decodar (provavelmente reusando algo que eu fiz,
-    #mas n sei)
-    pass
+           output = []
+           while pedaco:
+              # pdb.set_trace()
+               ped_lz4 = {
+                   'a': 4,
+                   'd': lz4.frame.compress(pedaco)
+               }
+               codec_huff = HuffmanCodec.from_data(pedaco)
+               ped_huff = {
+                   'a': 1,
+                   'd': codec_huff.encode(pedaco),
+                   't': codec_huff.get_code_table()
+               }
+               small_dict = pack_smaller(ped_lz4, ped_huff)
+               output.append(small_dict)
+               pedaco = input_file.read(CHUNK)
+           with open(args.output_filename, 'wb') as output_file:
+               umsgpack.pack(output, output_file, ext_handlers=handlers)
+    else:
+        output = bytearray()
+        input_list = umsgpack.unpack(input_file, ext_handlers={
+            0x30: lambda _: dahuffman.huffmancodec._EndOfFileSymbol()
+        })
+        for pedaco in input_list:
+            if pedaco['a'] == 4:
+                descompa = lz4.frame.decompress(pedaco['d'])
+            elif pedaco['a'] == 1:
+                codec = HuffmanCodec(pedaco['t'], check=False)
+                descompa = codec.decode(pedaco['d'])
+            else:
+                raise ValueError('chunck should not have a with value ' + str(pedaco['a']))
+            try:
+                output += descompa
+            except TypeError:
+                output += bytes(descompa)
+        with open(args.output_filename, 'wb') as output_file:
+            output_file.write(output)
+
 
 print('huffman %d' % huffmann_counter)
 print('lz4 %d' % lz4_counter)
